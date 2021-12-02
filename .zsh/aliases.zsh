51e4f54c295d06842b2464c55f03e3ee96ef13d7
Joseph Prinz <jp102@duke.edu>
2021-12-02 11:10:23 -0500

    Adding alias for dotfiles

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -16,6 +16,7 @@ alias viewdiff="vimdiff -R"
 alias tmux="tmux -2u"
 alias ddsc="export DDSCLIENT_CONF=$HOME/.ddsclient; ddsclient" 
 alias oddsc="export DDSCLIENT_CONF=$HOME/.ddsclient_omicscore; ddsclient" 
+alias dots="dotfiles"
 
 # SLURM Aliases that provide information in a useful manner for our clusters
 alias si="sinfo -o \"%20P %5D %14F %8z %10m %10d %11l %16f %N\""
